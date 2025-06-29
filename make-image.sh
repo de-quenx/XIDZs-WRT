7e758a0c47925326a951fd72f60f983f75dd2412
de-quenx <fidzxidz@gmail.com>
2025-06-29 23:09:40 +0700

    change make-image.sh

--- make-image.sh
+++ make-image.sh
@@ -15,20 +15,21 @@ PACKAGES=""
 
 # Hardware support packages - USB Networking drivers
 PACKAGES+=" kmod-usb-net-rtl8150 kmod-usb-net-rtl8152 kmod-usb-net-asix kmod-usb-net-asix-ax88179"
-PACKAGES+=" kmod-mii kmod-usb-net kmod-usb-wdm kmod-usb-net-rndis kmod-usb-net-sierrawireless kmod-usb-net-qmi-wwan uqmi"
-PACKAGES+=" kmod-usb-net-cdc-ether usb-modeswitch kmod-usb-acm kmod-usb-net-huawei-cdc-ncm kmod-usb-net-cdc-ncm"
-PACKAGES+=" kmod-usb-net-cdc-mbim umbim kmod-usb-serial-option kmod-usb-serial kmod-usb-serial-wwan kmod-usb-serial-qualcomm mbim-utils qmi-utils"
-PACKAGES+=" libqmi libmbim modemmanager luci-proto-modemmanager luci-proto-qmi xmm-modem usbutils"
-PACKAGES+=" kmod-usb-uhci kmod-usb-ohci kmod-usb2 kmod-usb-ehci kmod-usb3 kmod-macvlan"
+PACKAGES+=" kmod-mii kmod-usb-net kmod-usb-wdm kmod-usb-net-qmi-wwan uqmi luci-proto-qmi"
+PACKAGES+=" kmod-usb-net-cdc-ether kmod-usb-serial-option kmod-usb-serial kmod-usb-serial-wwan qmi-utils"
+PACKAGES+=" kmod-usb-serial-qualcomm kmod-usb-acm kmod-usb-net-cdc-ncm kmod-usb-net-cdc-mbim umbim"
+PACKAGES+=" modemmanager luci-proto-modemmanager libmbim libqmi usbutils luci-proto-ncm"
+PACKAGES+=" kmod-usb-net-huawei-cdc-ncm kmod-usb-net-cdc-ether kmod-usb-net-rndis kmod-usb-net-sierrawireless kmod-usb-ohci kmod-usb-serial-sierrawireless"
+PACKAGES+=" kmod-usb-uhci kmod-usb2 kmod-usb-ehci usb-modeswitch kmod-nls-utf8 mbim-utils xmm-modem kmod-macvlan"
 
 # Modem management tools
-PACKAGES+=" modeminfo luci-app-modeminfo atinout modemband luci-app-modemband luci-app-mmconfig sms-tool luci-app-sms-tool-js luci-app-3ginfo-lite picocom minicom"
+PACKAGES+=" modeminfo luci-app-modeminfo atinout modemband luci-app-modemband sms-tool luci-app-sms-tool-js picocom minicom"
 
 # ModemInfo serial support packages
 PACKAGES+=" modeminfo-serial-tw modeminfo-serial-dell modeminfo-serial-xmm modeminfo-serial-fibocom modeminfo-serial-sierra"
 
 # VPN tunnel package groups
-OPENCLASH="coreutils-nohup bash ca-certificates ipset ip-full libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag kmod-nft-tproxy luci luci-base luci-app-openclash"
+OPENCLASH="coreutils-nohup bash ca-certificates ipset ip-full libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag kmod-nft-tproxy luci-app-openclash"
 NIKKI="nikki luci-app-nikki"
 NEKO="bash kmod-tun php8 php8-cgi luci-app-neko"
 PASSWALL="chinadns-ng resolveip dns2socks dns2tcp ipt2socks microsocks tcping xray-core xray-plugin luci-app-passwall"
@@ -36,7 +37,6 @@ PASSWALL="chinadns-ng resolveip dns2socks dns2tcp ipt2socks microsocks tcping xr
 # Function to add requested tunnel packages
 add_tunnel_packages() {
     local option="$1"
-    
     case "$option" in
         "openclash")
             PACKAGES+=" $OPENCLASH"
@@ -48,8 +48,6 @@ add_tunnel_packages() {
             PACKAGES+=" $OPENCLASH $NIKKI $PASSWALL"
             ;;
     esac
-    
-    log "INFO" "Added tunnel option: $option"
 }
 
 # Storage and NAS functionality
@@ -65,13 +63,13 @@ PACKAGES+=" tailscale luci-app-tailscale"
 PACKAGES+=" speedtest-cli luci-app-eqosplus"
 
 # UI themes
-PACKAGES+=" luci-theme-argon luci-theme-alpha luci-theme-material"
+PACKAGES+=" luci-theme-argon luci-theme-material"
 
 # PHP support packages
 PACKAGES+=" libc php8 php8-fastcgi php8-fpm php8-mod-session php8-mod-ctype php8-mod-fileinfo php8-mod-zip php8-mod-iconv php8-mod-mbstring zoneinfo-core zoneinfo-asia"
 
 # Initialize variables for misc packages and exclusions
-MISC=""
+misc=""
 EXCLUDED=""
 
 # Add hardware-specific packages based on profile
@@ -80,49 +78,46 @@ configure_profile_packages() {
     
     # Add Raspberry Pi specific packages
     if [ "$profile_name" == "rpi-4" ]; then
-        MISC+=" kmod-i2c-bcm2835 i2c-tools kmod-i2c-core kmod-i2c-gpio"
-        log "INFO" "Added Raspberry Pi specific packages"
+        misc+=" kmod-i2c-bcm2835 i2c-tools kmod-i2c-core kmod-i2c-gpio"
     fi
     
     # Add x86_64 specific packages
     if [ "$ARCH_2" == "x86_64" ]; then
-        MISC+=" kmod-iwlwifi iw-full pciutils"
-        log "INFO" "Added x86_64 specific packages"
+        misc+=" kmod-iwlwifi iw-full pciutils"
     fi
     
     # Add build type specific packages
     if [[ "${TYPE}" == "OPHUB" ]]; then
-        MISC+=" kmod-fs-btrfs btrfs-progs luci-app-amlogic"
+        misc+=" luci-app-amlogic btrfs-progs kmod-fs-btrfs"
         EXCLUDED+=" -procd-ujail"
-        log "INFO" "Configured for OPHUB build type"
     elif [[ "${TYPE}" == "ULO" ]]; then
-        MISC+=" luci-app-amlogic"
+        misc+=" luci-app-amlogic"
         EXCLUDED+=" -procd-ujail"
-        log "INFO" "Configured for ULO build type"
     fi
 }
 
 # Add base release specific packages
 configure_release_packages() {
     if [ "${BASE}" == "openwrt" ]; then
-        MISC+=" wpad-openssl iw iwinfo wireless-regdb kmod-cfg80211 kmod-mac80211 luci-app-temp-status"
+        misc+=" wpad-openssl iw iwinfo wireless-regdb kmod-cfg80211 kmod-mac80211 luci-app-temp-status"
         EXCLUDED+=" -dnsmasq"
-        log "INFO" "Configured for OpenWrt base"
     elif [ "${BASE}" == "immortalwrt" ]; then
-        MISC+=" wpad-openssl iw iwinfo wireless-regdb kmod-cfg80211kmod-mac80211"
+        misc+=" wpad-openssl iw iwinfo wireless-regdb kmod-cfg80211 kmod-mac80211"
         EXCLUDED+=" -dnsmasq -cpusage -automount -libustream-openssl -default-settings-chn -luci-i18n-base-zh-cn"
         if [ "$ARCH_2" == "x86_64" ]; then
-        EXCLUDED+=" -kmod-usb-net-rtl8152-vendor"
+            EXCLUDED+=" -kmod-usb-net-rtl8152-vendor"
+        elif [ "$ARCH_3" == "i386_pentium4" ]; then
+            EXCLUDED+=" -kmod-usb-net-rtl8152-vendor"
         fi
-        log "INFO" "Configured for ImmortalWrt base"
     fi
 }
 
-# Add common utility packages
-MISC+=" coreutils-base64 coreutils-sleep coreutils-stat coreutils-stty block-mount losetup parted resize2fs"
-MISC+=" bash luci luci-ssl uhttpd uhttpd-mod-ubus curl wget-ssl tar unzip jq httping python3-pip zram-swap"
-MISC+=" adb htop screen lolcat luci-app-poweroffdevice luci-app-ramfree luci-app-ttyd luci-app-lite-watchdog"
-MISC+=" luci-app-ipinfo luci-app-droidnet luci-app-mactodong luci-app-tinyfm"
+# Common utility packages
+misc+=" adb bash block-mount coreutils-base64 coreutils-sleep coreutils-stat coreutils-stty curl"
+misc+=" htop httping jq libc losetup lolcat luci luci-ssl parted python3-pip"
+misc+=" resize2fs screen tar uhttpd uhttpd-mod-ubus unzip wget-ssl zram-swap"
+misc+=" luci-app-droidnet luci-app-ipinfo luci-app-lite-watchdog luci-app-mactodong"
+misc+=" luci-app-poweroffdevice luci-app-ramfree luci-app-tinyfm luci-app-ttyd luci-app-3ginfo-lite luci-app-mmconfig"
 
 # Main firmware build function
 build_firmware() {
@@ -130,20 +125,12 @@ build_firmware() {
     local tunnel_option="$2"
     local build_files="files"
     
-    log "INFO" "==============================================="
-    log "INFO" "Starting build for profile: $target_profile"
-    log "INFO" "Tunnel option: $tunnel_option"
-    log "INFO" "==============================================="
-    # Configure packages based on profile and options
     configure_profile_packages "$target_profile"
     add_tunnel_packages "$tunnel_option"
     configure_release_packages
     
-    # Add misc packages to the main package list
-    PACKAGES+=" $MISC"
-    log "INFO" "Building image with profile: $target_profile"
+    PACKAGES+=" $misc"
     
-    # Execute the build command
     make image PROFILE="$target_profile" PACKAGES="$PACKAGES $EXCLUDED" FILES="$build_files" 2>&1
     local build_status=${PIPESTATUS[0]}
     if [ $build_status -eq 0 ]; then
